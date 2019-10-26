d59aec3dbb183d154dfefa1411c7d4340e3ed236
Heverton Sarah <heverton.sarah@gmail.com>
2019-10-26 09:52:02 -0300

    Initial commit

---
+++ SearchTreeProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SearchTreeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
